b4fd46a33a2d58809cb3b5da5e572bc01046ee33
Sasha-Maniak <116157371+metalikaqq@users.noreply.github.com>
2023-11-08 17:29:00 +0200

    https 1

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 require('dotenv').config();
 
+const fs = require('fs');
+const https = require('https');
 const app = require('./app');
 
 const port = process.env.PORT || 5000;
+
+const certificate = fs.readFileSync('../certificate.crt');
+const privateKey = fs.readFileSync('../private.key');
+
+try {
+  https.createServer({
+    key: privateKey,
+    cert: certificate
+  }, app).listen(8443);
+
+  console.log('HTTPS Server running on port 8443');
+} catch (error) {
+  console.error(error);
+}
 
 try {
   app.listen(port, () => console.log(`Server running on port ${port}`));
